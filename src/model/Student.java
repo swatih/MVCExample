8ba885108ab571a8ee5d68eec08d4a80651bffb8
swati hans <swati1hans@gmail.com>
2016-12-24 01:07:22 +0530

    first upload

---
+++ src/model/Student.java
@@ -0,0 +1,33 @@
+package model;
+
+
+public class Student {
+	String name;
+ int id;
+ String grade;
+	public int getId() {
+	return id;
+}
+
+public void setId(int id) {
+	this.id = id;
+}
+
+public String getGrade() {
+	return grade;
+}
+
+public void setGrade(String grade) {
+	this.grade = grade;
+}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
---
+++ src/servlets/Controller2.java
@@ -0,0 +1,56 @@
+package servlets;
+import model.*;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Controller
+ */
+@WebServlet("/Controller2")
+public class Controller2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Controller2() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String n=request.getParameter("t1");
+		Student s=new Student();
+		RequestDispatcher rd=request.getRequestDispatcher("/details.jsp");
+		s.setGrade("a");
+		s.setId(2);
+		s.setName(n);
+		request.setAttribute("model",s);
+	    rd.forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
